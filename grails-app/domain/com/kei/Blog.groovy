b852df0f5294f57fffe03e43070ea0f88461244a
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-11-05 14:59:53 +0900

    Blog.groovy modified such that comment can be included [1:n]

--- grails-app/domain/com/kei/Blog.groovy
+++ grails-app/domain/com/kei/Blog.groovy
@@ -2,6 +2,13 @@ package com.kei
 
 class Blog {
 
+    String title
+    String content
+    Date   dateCreated
+    Date   dateUpdated
+
+    static hasMany     = [comments:Comment]
     static constraints = {
+         
     }
 }
--- grails-app/domain/com/kei/Comment.groovy
+++ grails-app/domain/com/kei/Comment.groovy
@@ -1,7 +1,7 @@
 package com.kei
 
-class Main {
+class Comment {
+
     static constraints = {
-        
     }
 }
