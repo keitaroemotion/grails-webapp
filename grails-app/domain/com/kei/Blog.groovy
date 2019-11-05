45e0bb57cb1588203a38eafe49c32fc361b8eb71
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-11-05 15:11:53 +0900

    Comment domain model updated

--- grails-app/domain/com/kei/Blog.groovy
+++ grails-app/domain/com/kei/Blog.groovy
@@ -7,7 +7,7 @@ class Blog {
     Date   dateCreated
     Date   dateUpdated
 
-    static hasMany     = [comments:Comment]
+    static hasMany     = [comments: Comment]
     static constraints = {
         title  (blank: false, nullable: false, size: 3..80)
         content(blank: false, nullable: false, size: 3..500)
--- grails-app/domain/com/kei/Comment.groovy
+++ grails-app/domain/com/kei/Comment.groovy
@@ -2,6 +2,19 @@ package com.kei
 
 class Comment {
 
+    String comment
+    Date   dateCreated
+    Date   dateUpdated
+
+    static belongsTo   = [blog: Blog]
     static constraints = {
+        comment(blank: false, nullable: false, size: 5..500)
+    }
+
+    String toString() {
+        if(comment?.size) > 20) {
+            return comment.substring(0, 20)
+        } 
+        return comment
     }
 }
